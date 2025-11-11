7108f67dba7093ffa6765ba543d628e0b628d8c4
tannerjackley <s561664@nwmissouri.edu>
2025-11-11 12:12:18 -0600

    calls to TMDB API

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "395d5ac6464a7d8b51e604d14db22a98";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
